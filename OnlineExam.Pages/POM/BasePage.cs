27ac26d8a7fc84497ddf839c40464f906db04728
Inna <inna.syrotiuk@gmail.com>
2018-06-22 17:46:22 +0300

    Add project files.

---
+++ OnlineExam.Pages/POM/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Pages.POM
+{
+  public abstract  class BasePage
+    {
+        protected IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+    }
+}
