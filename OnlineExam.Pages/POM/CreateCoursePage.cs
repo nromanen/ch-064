691415de3abd86ef449c13b7f7142f44a0d6a6ae
Roma Bahlai <feared@ex.ua>
2018-06-25 19:23:44 +0300

    initial commit by Roma

---
+++ OnlineExam.Pages/POM/CreateCoursePage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Pages.POM
+{
+    public class CreateCoursePage : BasePage
+    {
+        /// <summary>
+        /// http://localhost:55842/CourseManagement/Create
+        /// </summary>
+
+        public CreateCoursePage(IWebDriver driver) : base(driver) { }
+
+        [FindsBy(How = How.Id, Using = "Name")]
+        public IWebElement CourseNameInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Description")]
+        public IWebElement CourseDescriptionInput { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input.btn")]
+        public IWebElement CourseOkBtn { get; set; }
+    }
+}
