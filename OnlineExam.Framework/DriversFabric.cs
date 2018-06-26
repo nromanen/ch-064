1574545ed26c2f9458df067e1bc7a21c14e29746
Inna <inna.syrotiuk@gmail.com>
2018-06-26 19:23:02 +0300

    commit to pull

---
+++ OnlineExam.Framework/DriversFabric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OnlineExam.Framework
+{
+    public class DriversFabric
+    {
+        public static ExtendedWebDriver InitChrome()
+        {
+            
+            IWebDriver driver = new ChromeDriver();
+            return  new ExtendedWebDriver(driver);
+        }
+    }
+}
