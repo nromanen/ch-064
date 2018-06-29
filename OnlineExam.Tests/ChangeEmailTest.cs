7973b1f2774139d606759c00072e787a9b117834
Inna <inna.syrotiuk@gmail.com>
2018-06-29 14:52:45 +0300

    ChangeEmailTest1

--- OnlineExam.Tests/ChangeEmailTest.cs
+++ OnlineExam.Tests/ChangeEmailTest.cs
@@ -17,23 +17,7 @@ namespace OnlineExam.Tests
         [Fact]
         public void TestChangeEmail()
         {
-            BeginTest();
-            driver.Navigate().GoToUrl("http://localhost:55842/Account/Login");
-            System.Threading.Thread.Sleep(1000);
-
-            var loginPage = new LogInPage(driver);
-            var indexPage = loginPage.SignIn("student@gmail.com", Constants.STUDENT_PASSWORD);
-            var header = new Header(driver);
-            var userInfo = header.GoToUserAccountPage();
-            var changeNamePage = userInfo.OpenChangeNamePage();
-
-            changeNamePage.SetNewName("studentTEST", Constants.STUDENT_PASSWORD);
-            changeNamePage.SaveNewName();
-
-            header = new Header(driver);
-            var newUserName = header.GetHeaderUserName();
-            var isEqual = String.Equals("studentTEST", newUserName, StringComparison.InvariantCultureIgnoreCase);
-            Assert.True(isEqual);
+        
         }
     }
 }
