84e9498f50088ddb2200ed87ccffb0134941ecd8
Inna <inna.syrotiuk@gmail.com>
2018-07-06 17:46:36 +0300

    Create test CheckContentUserInfoPage

---
+++ OnlineExam.Tests/CheckContentUserInfoPage.cs
@@ -0,0 +1,44 @@
+﻿using OnlineExam.Pages.POM;
+using OnlineExam.Pages.POM.UserDetails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineExam.Tests
+{
+    public class CheckContentUserInfoPage : BaseTest
+    {
+        public CheckContentUserInfoPage()
+        { 
+        }
+
+        [Fact]
+        public void TestChangeName()
+        {
+            BeginTest();
+            driver.Navigate().GoToUrl("http://localhost:55842/Account/Login");
+            System.Threading.Thread.Sleep(1000);
+            var loginPage = ConstructPage<LogInPage>();
+            var indexPage = loginPage.SignIn("student3@gmail.com", Constants.STUDENT_PASSWORD);
+            var header = ConstructPage<Header>();
+            var userInfo = header.GoToUserAccountPage();
+
+            var isOpened = driver.Url.EndsWith("/User");
+            Assert.True(isOpened);
+
+            var userInfoPage = ConstructPage<UserInfoPage>();
+            Assert.True(userInfoPage.HasChangePasswordButton());
+            Assert.True(userInfoPage.HasChangeNameButton());
+            Assert.True(userInfoPage.HasChangeEmailButton());
+
+            var userEmail = userInfoPage.GetEmail();
+            var isEqual = String.Equals(userEmail, "student3@gmail.com");
+            Assert.True(isEqual);
+
+
+        }
+    }
+}
