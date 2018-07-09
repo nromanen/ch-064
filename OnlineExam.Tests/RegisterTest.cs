4c6263eb7b3d32b753deb8d6bc89bf51fe611e10
Surikenchik <popelyshkon25@gmail.com>
2018-07-09 17:17:19 +0300

    NewsTest fixed & is working clear. :ok_woman: ITS A WIN

--- OnlineExam.Tests/RegisterTest.cs
+++ OnlineExam.Tests/RegisterTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using AventStack.ExtentReports;
 using OnlineExam.Pages.POM;
 using Xunit;
 
@@ -12,8 +13,14 @@ namespace OnlineExam.Tests
     [Collection("MyTestCollection")]
     public class RegisterTest : BaseTest
     {
-        public RegisterTest()
+        private Header header;
+        private SideBar sideBar;
+
+        public RegisterTest(BaseFixture fixture) : base(fixture)
         {
+            BeginTest();
+            header = ConstructPage<Header>();
+            sideBar = ConstructPage<SideBar>();
         }
 
         [Fact]
@@ -21,8 +28,7 @@ namespace OnlineExam.Tests
         {
             UITest(() =>
             {
-                BeginTest();
-                var header = ConstructPage<Header>();
+                fixture.test = fixture.extentReports.CreateTest("CheckIfUserIsPresentedInUserListAfterSignUp");
                 var signUp = header.GoToRegistrationPage();
                 signUp.Registration(Constants.EXAMPLE_EMAIL, Constants.EXAMPLE_PASSWORD, Constants.EXAMPLE_PASSWORD);
                 header.SignOut();
@@ -31,6 +37,7 @@ namespace OnlineExam.Tests
                 var adminPanelPage = ConstructPage<SideBar>().GoToAdminPanelPage();
                 Assert.True(adminPanelPage.IsUserPresentedInUserList(Constants.EXAMPLE_EMAIL));
                 adminPanelPage.DeleteUser(Constants.EXAMPLE_EMAIL);
+                fixture.test.Log(Status.Pass, "User Is Presented In User List After SignUp.");
             });
         }
 
@@ -39,12 +46,12 @@ namespace OnlineExam.Tests
         {
             UITest(() =>
             {
-                BeginTest();
-                var header = ConstructPage<Header>();
+                fixture.test = fixture.extentReports.CreateTest("SignUpAsUsedEmail");
                 var signUp = header.GoToRegistrationPage();
                 signUp.Registration(Constants.STUDENT_EMAIL, Constants.EXAMPLE_PASSWORD, Constants.EXAMPLE_PASSWORD);
                 header.GoToHomePage();
                 Assert.True(header.IsUserEmailPresentedInHeader(Constants.STUDENT_EMAIL));
+                fixture.test.Log(Status.Fail, "Didn't sign up as used email.");
             });
         }
 
