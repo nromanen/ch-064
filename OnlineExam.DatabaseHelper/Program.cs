ec9327c8269692cd1aac2047a1baa49fa2360447
Roma Bahlai <feared@ex.ua>
2018-07-16 23:37:29 +0300

    Create new console app project

---
+++ OnlineExam.DatabaseHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.DatabaseHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type 'help' for more info.");
+            Console.Write(">");
+            Console.ReadKey();
+        }
+    }
+}
