a6b148bad8c3acb5b47607ab22e2d34a61ca1491
Surikenchik <popelyshkon25@gmail.com>
2018-07-17 17:58:48 +0300

    removed uitest

--- OnlineExam.NUnitTests/RegistrationNTest.cs
+++ OnlineExam.NUnitTests/RegistrationNTest.cs
@@ -26,29 +26,22 @@ namespace OnlineExam.NUnitTests
         [Test]
         public void CheckIfUserIsPresentedInUserListAfterSignUp()
         {
-            UITest(() =>
-            {
-                var signUp = header.GoToRegistrationPage();
-                signUp.Registration(Constants.EXAMPLE_EMAIL, Constants.EXAMPLE_PASSWORD, Constants.EXAMPLE_PASSWORD);
-                header.SignOut();
-                var logIn = header.GoToLogInPage();
-                logIn.SignIn(Constants.ADMIN_EMAIL, Constants.ADMIN_PASSWORD);
-                var adminPanelPage = ConstructPage<SideBar>().GoToAdminPanelPage();
-                Assert.True(adminPanelPage.IsUserPresentedInUserList(Constants.EXAMPLE_EMAIL));
-            });
+            var signUp = header.GoToRegistrationPage();
+            signUp.Registration(Constants.EXAMPLE_EMAIL, Constants.EXAMPLE_PASSWORD, Constants.EXAMPLE_PASSWORD);
+            header.SignOut();
+            var logIn = header.GoToLogInPage();
+            logIn.SignIn(Constants.ADMIN_EMAIL, Constants.ADMIN_PASSWORD);
+            var adminPanelPage = ConstructPage<SideBar>().GoToAdminPanelPage();
+            Assert.True(adminPanelPage.IsUserPresentedInUserList(Constants.EXAMPLE_EMAIL));
         }
 
         [Test]
         public void SignUpAsUsedEmail()
         {
-            UITest(() =>
-            {
-                var signUp = header.GoToRegistrationPage();
-                signUp.Registration(Constants.STUDENT_EMAIL, Constants.EXAMPLE_PASSWORD, Constants.EXAMPLE_PASSWORD);
-                header.GoToHomePage();
-                Assert.False(header.IsUserEmailPresentedInHeader(Constants.STUDENT_EMAIL));
-
-            });
+            var signUp = header.GoToRegistrationPage();
+            signUp.Registration(Constants.STUDENT_EMAIL, Constants.EXAMPLE_PASSWORD, Constants.EXAMPLE_PASSWORD);
+            header.GoToHomePage();
+            Assert.False(header.IsUserEmailPresentedInHeader(Constants.STUDENT_EMAIL));
         }
     }
 }
