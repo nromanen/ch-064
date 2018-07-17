cbc7c6f0af3da25f532d62de3a7d4649b14d0d66
viktor <viktor140798@gmail.com>
2018-07-17 17:00:11 +0300

    commit

--- OnlineExam.NUnitTests/AdminNTest.cs
+++ OnlineExam.NUnitTests/AdminNTest.cs
@@ -21,49 +21,40 @@ namespace OnlineExam.NUnitTests
             logInPage = header.GoToLogInPage();
             logInPage.SignIn(Constants.ADMIN_EMAIL, Constants.ADMIN_PASSWORD);
             adminPanelPage = ConstructPage<SideBar>().GoToAdminPanelPage();
-        } 
-        
-   
+        }
+
+
         [Test]
         public void IsUserPresentedInUserListTest()
         {
-            
-                Assert.True(adminPanelPage.IsUserPresentedInUserList(Constants.STUDENT_EMAIL));
+            Assert.True(adminPanelPage.IsUserPresentedInUserList(Constants.STUDENT_EMAIL));
         }
 
         [Test]
         public void DeleteUserTest()
         {
-            UITest(() =>
-            {
-                Assert.True(adminPanelPage.IsUserPresentedInUserList(Constants.USER_FOR_DELETE_EMAIL),
-                    "User is not presented in the system," +
-                    "so we have not opportunity to delete this user");
-                adminPanelPage.DeleteUser(Constants.USER_FOR_DELETE_EMAIL);
+            Assert.True(adminPanelPage.IsUserPresentedInUserList(Constants.USER_FOR_DELETE_EMAIL),
+                "User is not presented in the system," +
+                "so we have not opportunity to delete this user");
+            adminPanelPage.DeleteUser(Constants.USER_FOR_DELETE_EMAIL);
 
-                Assert.True(adminPanelPage.IsListOfUsersH2ElementPresented());
-                Assert.False(adminPanelPage.IsUserPresentedInUserList(Constants.USER_FOR_DELETE_EMAIL), "Error");
-
-            });
+            Assert.True(adminPanelPage.IsListOfUsersH2ElementPresented());
+            Assert.False(adminPanelPage.IsUserPresentedInUserList(Constants.USER_FOR_DELETE_EMAIL), "Error");
         }
 
         [Test]
         public void ChangeUserRoleTest()
         {
-            UITest(() =>
-            {
-                var changeRolePage = adminPanelPage.ChangeRoleOfUserButtonClick(Constants.USER_FOR_CHANGE_ROLE_EMAIL);
-                changeRolePage.ChangeRole(Constants.TEACHER);
-                changeRolePage = adminPanelPage.ChangeRoleOfUserButtonClick(Constants.USER_FOR_CHANGE_ROLE_EMAIL);
-                Assert.AreEqual(Constants.TEACHER, changeRolePage.CurrentRole());
-            });
+            var changeRolePage = adminPanelPage.ChangeRoleOfUserButtonClick(Constants.USER_FOR_CHANGE_ROLE_EMAIL);
+            changeRolePage.ChangeRole(Constants.TEACHER);
+            changeRolePage = adminPanelPage.ChangeRoleOfUserButtonClick(Constants.USER_FOR_CHANGE_ROLE_EMAIL);
+            Assert.AreEqual(Constants.TEACHER, changeRolePage.CurrentRole());
         }
 
         [Test]
         public void IsUserListAvailableTest()
         {
-            
-                Assert.True(adminPanelPage.IsListOfUsersH2ElementPresented());
+            Assert.True(adminPanelPage.IsListOfUsersH2ElementPresented());
         }
-        }
+    }
 }
