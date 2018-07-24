36e5cc63f5254ec1ce0759bf6f8c99a3965fb2b8
m1shkka <mishamaxixi@gmail.com>
2018-07-24 17:14:04 +0300

    some changes and attempts for creating api tests

--- OnlineExam.NUnitTests/RestClients/Courses.cs
+++ OnlineExam.NUnitTests/RestClients/Courses.cs
@@ -1,87 +1,73 @@
-﻿using System;
-using System.Linq;
-using NUnit.Framework;
-using RestSharp;
-using RestSharpCore.model;
+﻿//using System;
+//using System.Linq;
+//using NUnit.Framework;
+//using RestSharp;
 
+//namespace OnlineExam.NUnitTests.RestClients
+//{
+//    public class Courses
+//    {
+//        public int id { get; set; }
+//        public string CreationDate { get; set; }
+//        public string Description { get; set; }
+//        public bool IsActive { get; set; }
+//        public string name { get; set; }
+//        public string UserId { get; set; }
+//    }
 
-namespace OnlineExam.NUnitTests.RestClients
-{
-    public class Courses
-    {
-        public int id { get; set; }
-        public string CreationDate { get; set; }
-        public string Description { get; set; }
-        public bool IsActive { get; set; }
-        public string name { get; set; }
-        public string UserId { get; set; }
-    }
+//    [TestFixture]
+//    public class CoursesTest
+//    {
+//        [Test]
+//        public void CoursesID()
+//        {
+//            var client = new RestClient();
+//            client.BaseUrl = new Uri("http://localhost:55842");
 
-    [TestFixture]
-    public class CoursesTest
-    {
-        [Test]
-        public void CommentWhereIDEquals1()
-        {
-            var hz = new RestSharp.RestClient();
-            var client = new RestClient();
-            client.BaseUrl = new Uri("http://jsonplaceholder.typicode.com/");
+//            var request = new RestRequest("/api/Course", Method.GET);
+//            request.AddUrlSegment("id", "");
 
-            var request = new RestRequest("comments/1", Method.GET);
-            request.AddUrlSegment("id", "1");
+//            var responseCourseModel = client.Execute<Courses>(request);
+//            var result = responseCourseModel.Content;
+//            Assert.NotNull(result);
 
-            var responseCommentModel = client.Execute<Comment>(request);
+//           // Assert.AreEqual(1, responseCourseModel.Data.id);//string.Format("ID {0} does not match actually {1}", 1, responseCourseModel.Data.id));
+//        }
 
-            Assert.AreEqual(1, responseCommentModel.Data.id,
-                string.Format("ID {0} does not match actually {1}", 1, responseCommentModel.Data.id));
-        }
+//        [Test]
+//        public void CourseCreationDate()
+//        {
+//            var client = new RestClient();
+//            client.BaseUrl = new Uri("http://localhost:55842");
 
-        [Test]
-        public void CommentWherePostIdEquals1()
-        {
-            var client = new RestClient();
-            client.BaseUrl = new Uri("http://jsonplaceholder.typicode.com/");
+//            var request = new RestRequest("/CourseManagement", Method.GET);
+//            request.AddUrlSegment("id", "1");
 
-            var request = new RestRequest("comments/1", Method.GET);
-            request.AddUrlSegment("id", "1");
+//            IRestResponse<Courses> responseCommentModel = client.Execute<Courses>(request);
 
-            IRestResponse<Comment> responseCommentModel = client.Execute<Comment>(request);
+//            Assert.AreEqual(1, responseCommentModel.Data.CreationDate,
+//                string.Format("PostID {0} does not match actually {1}", 1, responseCommentModel.Data.CreationDate));
+//        }
 
-            Assert.AreEqual(1, responseCommentModel.Data.postId,
-                string.Format("PostID {0} does not match actually {1}", 1, responseCommentModel.Data.postId));
-        }
+//        [Test]
+//        public void CoursesPOST()
+//        {
+//            var client = new RestClient();
+//            client.BaseUrl = new Uri("http://localhost:55842");
+//            var loginRequest = new RestRequest("/swagger.json#/Course/Course_Create", Method.POST);
+//            loginRequest.AddHeader("content-type", "application/json");
+//            string json = @"{
+//             ""Id"": 11,
+//             ""CreationDate"":""2018-07-24 16:02:30.9193083"",
+//             ""Description"" : ""camon"",
+//             ""IsActive"" : ""True"",
+//             ""Name"" : ""NAME"",
+//             ""UserId"" : ""e43b63ea-3492-454b-9517-8c62ae65c598""
+//                }";
 
-
-        [Test]
-        public void CommentWhereBodyContainsIspam()
-        {
-            var client = new RestClient();
-            client.BaseUrl = new Uri("http://jsonplaceholder.typicode.com/");
-
-            var request = new RestRequest("comments/1", Method.GET);
-            request.AddUrlSegment("id", "1");
-
-            IRestResponse<Comment> responseCommentModel = client.Execute<Comment>(request);
-            var assert = responseCommentModel.Data.body.Contains("ipsam");
-            Assert.IsTrue(assert);
-        }
-
-
-        [Test]
-        public void CommentEmailTest()
-        {
-            var client = new RestClient();
-            client.BaseUrl = new Uri("http://jsonplaceholder.typicode.com/");
-
-            var request = new RestRequest("comments/1", Method.GET);
-            request.AddUrlSegment("id", "1");
-
-            IRestResponse<Comment> responseCommentModel = client.Execute<Comment>(request);
-
-            Assert.AreEqual("Eliseo@gardner.biz", responseCommentModel.Data.email,
-                string.Format("Email {0} does not match {1}", "Eliseo@gardner.biz", responseCommentModel.Data.body));
-        }
-    }
-
-
-}
+//            loginRequest.AddParameter("text/json", json, ParameterType.RequestBody);
+//            IRestResponse response = client.Execute(loginRequest);
+//            Assert.NotNull(response);
+//        }
+//    }
+//}
