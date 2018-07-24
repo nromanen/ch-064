f48cc7d1cb86655faf1cd3551d915b6426d81bd5
viktor <viktor140798@gmail.com>
2018-07-24 17:03:08 +0300

    api testing

---
+++ OnlineExam.NUnitTests/APIClients/APIUsersClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace OnlineExam.NUnitTests.APIClients
+{
+    public class APIUsersClient
+    {
+        RestClient client = new RestClient("http://localhost:55842/swagger/v1");
+
+       
+        public string PostChange(string json,string parameter)
+        {
+            var request = new RestRequest("/api/users/change/name", Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("text/json", json, ParameterType.RequestBody);
+
+            
+            IRestResponse response = client.Execute(request);
+            var result = response.StatusDescription;
+            return result;
+        }
+    }
+}
---
+++ OnlineExam.NUnitTests/APITests/APIUsersTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OnlineExam.NUnitTests.APIClients;
+
+namespace OnlineExam.NUnitTests.APITests
+{
+    [TestFixture]
+    public class APIUsersTest
+    {
+        private string json = @"{""id"":""88d0300e-0520-447c-b117-cde051119565"",""email"":""UserForChangeNameAPI@gmail.com"", ""password"": ""User_123"", ""newUserName"": ""ApiTest"",}";
+
+
+
+
+        private APIUsersClient client;
+
+
+
+        [Test]
+        public void ChangeNameTest()
+        {
+            client = new APIUsersClient();
+            var result = client.PostChange(json, "name");
+            Assert.NotNull(result);
+        }
+    }
+}
