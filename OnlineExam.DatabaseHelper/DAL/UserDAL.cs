2f38ea57de24ff90a5492f5866995e253b76c7a8
viktor <viktor140798@gmail.com>
2018-07-26 23:16:14 +0300

    added some api test

--- OnlineExam.DatabaseHelper/DAL/UserDAL.cs
+++ OnlineExam.DatabaseHelper/DAL/UserDAL.cs
@@ -8,9 +8,12 @@ namespace OnlineExam.DatabaseHelper.DAL
 {
     public class UserDAL
     {
+        private static string conection = "data source = DESKTOP-424095L\\SQLEXPRESS; initial catalog = OnlineExamDB; integrated security = True; MultipleActiveResultSets = True;";
+
+
         public AspNetUsers GetUserById(string id)
         {
-            using (var ctx = new DataModel())
+            using (var ctx = new DataModel(conection))
             {
                 //var result = ctx.AspNetUsers.Where(searchUser => searchUser.Id.Equals(id)).First();
                 var result = ctx.AspNetUsers.Find(id);
--- OnlineExam.NUnitTests/APITests/APIUsersTest.cs
+++ OnlineExam.NUnitTests/APITests/APIUsersTest.cs
@@ -14,24 +14,45 @@ namespace OnlineExam.NUnitTests.APITests
     [TestFixture]
     public class APIUsersTest
     {
-
         private APIUsersClient client;
-        
+
+        [SetUp]
+        public void SetUp()
+        {
+            client = new APIUsersClient();
+        }
+
+
         [Test]
         public void ChangeNameTest()
         {
             var obj = new
             {
-                id = "88d0300e-0520-447c-b117-cde051119565",
+                id = "7a72f372-b3bd-4391-b075-1510e34b98da",
                 email = "UserForChangeNameAPI@gmail.com",
                 password = "User_123",
-                newUserName = "ApiTest"
+                newUserName = "ApiTestNew"
             };
-            client = new APIUsersClient();
-            var result = client.PostChange(obj,"name");
-            Assert.AreEqual(result, "OK");
+            var result = client.PostChange(obj, "name");
+            Assert.AreEqual("OK", result);
             var actual = new UserDAL().GetUserById(obj.id);
-            Assert.AreEqual(actual.UserName,obj.newUserName);
+            Assert.AreEqual(actual.UserName, obj.newUserName);
+        }
+
+        [Test]
+        public void ChangeEmailTest()
+        {
+            var obj = new
+            {
+                id = "6734ae72-eb95-46f0-82fe-4bf62ea52210",
+                oldEmail = "UserForChangeEmailAPI@gmail.com",
+                password = "User_123",
+                newEmail = "UserTestAPI@gmail.com"
+            };
+            var result = client.PostChange(obj, "email");
+            Assert.AreEqual("OK", result);
+            var actual = new UserDAL().GetUserById(obj.id);
+            Assert.AreEqual(obj.newEmail, actual.Email);
         }
     }
 }
