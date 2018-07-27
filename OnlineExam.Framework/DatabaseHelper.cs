704af127138894cccf0dad1eeeb9e54a6665d100
m1shkka <mishamaxixi@gmail.com>
2018-07-27 17:50:32 +0300

    added ConnectingString for SQL in FabricDriver, serializing from json file

--- OnlineExam.Framework/DatabaseHelper.cs
+++ OnlineExam.Framework/DatabaseHelper.cs
@@ -9,7 +9,7 @@ namespace OnlineExam.Framework
 {
     public class DatabaseHelper
     {
-        private static string conection = "data source = DESKTOP-424095L\\SQLEXPRESS; initial catalog = OnlineExamDB; integrated security = True; MultipleActiveResultSets = True;";
+        private static string conection = DriversFabric.GetConnectionString();
         public static void BackupDatabase()
         {
             string backupLocation = System.IO.Path.Combine(Constants.PROJECT_PATH, "Backup", "Main.bak");
