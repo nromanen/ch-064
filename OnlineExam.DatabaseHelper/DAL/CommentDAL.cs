70e298652c392a490f8109719635f5166e22d12c
Surikenchik <popelyshkon25@gmail.com>
2018-07-27 17:45:16 +0300

    Started changing api

---
+++ OnlineExam.DatabaseHelper/DAL/CommentDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.DatabaseHelper.DAL
+{
+    public class CommentDAL
+    {
+        private static string conection = "data source = DESKTOP-7VG2H7M\\SQLEXPRESS; initial catalog = OnlineExamDB; integrated security = True; MultipleActiveResultSets = True;";
+
+        public Comments GetCommentById(string id)
+        {
+            using (var ctx = new DataModel(conection))
+            {
+                //var result = ctx.AspNetUsers.Where(searchUser => searchUser.Id.Equals(id)).First();
+                var result = ctx.Comments.Find(id);
+                return result;
+
+            }
+        }
+    }
+}
