51d2c8f4bf9574052de5ece5a23304755da8ba18
viktor <viktor140798@gmail.com>
2018-08-04 14:06:36 +0300

    Parallel test

--- OnlineExam.NUnitTests/CheckAnyInFavouritesN.cs
+++ OnlineExam.NUnitTests/CheckAnyInFavouritesN.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace OnlineExam.NUnitTests
 {
+    [Parallelizable(ParallelScope.Self)]
     [TestFixture]
     public class CheckAnyInFavouritesN : BaseNTest
     {
--- OnlineExam.NUnitTests/SaveExecutedTaskToCodeHistoryN.cs
+++ OnlineExam.NUnitTests/SaveExecutedTaskToCodeHistoryN.cs
@@ -9,71 +9,70 @@ using System.Threading.Tasks;
 
 namespace OnlineExam.NUnitTests
 {
+    [Parallelizable(ParallelScope.Self)]
     [TestFixture]
-    
-            public class TestSaveExecutedTestToCodeHistoryN : BaseNTest
-            {
-                private Header header;
-                private LogInPage logInPage;
-                private SideBar sideBar;
+    public class TestSaveExecutedTestToCodeHistoryN : BaseNTest
+    {
+        private Header header;
+        private LogInPage logInPage;
+        private SideBar sideBar;
 
 
         [SetUp]
-                public override void SetUp()
-                {
+        public override void SetUp()
+        {
             base.SetUp();
             header = ConstructPage<Header>();
-                }
+        }
 
-                [Test]
-                public void SaveExecutedTestToCodeHistory()
+        [Test]
+        public void SaveExecutedTestToCodeHistory()
+        {
+            var logInPage = header.GoToLogInPage();
+            logInPage.SignIn("student3@gmail.com", Constants.STUDENT_PASSWORD);
+            sideBar = ConstructPage<SideBar>();
+            var courseManagment = sideBar.GoToCourseManagementPage();
+            var coursesBlocks = courseManagment.GetBlocks();
+            var courseItem = coursesBlocks.FirstOrDefault(x =>
+                x.GetCourseName().Equals("C# Essential", StringComparison.OrdinalIgnoreCase));
+            courseItem.ClickCourseLink();
+
+            var ListOfTasks = ConstructPage<TasksPage>();
+            string TaskName = "Indexers";
+            string nameOfExecutedTask = "";
+
+
+            var blocks = ListOfTasks.GetBlocks();
+            if (blocks != null)
+            {
+                var firstblock =
+                    blocks.FirstOrDefault(x => x.GetName().Equals(TaskName, StringComparison.OrdinalIgnoreCase));
+                nameOfExecutedTask = firstblock.GetName();
+                firstblock.ClickOnTasksButton();
+
+                var TaskView = ConstructPage<TaskViewPage>();
+                TaskView.ClickOnStartButton();
+
+                var Code = ConstructPage<SolutionCodePage>();
+
+                Code.ClickOnExecuteButton("Total");
+            }
+
+            var historyPage = sideBar.GoToCodeHistoryPage();
+            var blocksHistory = historyPage.GetHistoryBlocks();
+
+            bool blockOfExecutedCode = false;
+
+            foreach (var block in blocksHistory)
+            {
+                if (block.GetTitle() == nameOfExecutedTask)
                 {
-
-
-                    var logInPage = header.GoToLogInPage();
-                    logInPage.SignIn("student3@gmail.com", Constants.STUDENT_PASSWORD);
-                    sideBar = ConstructPage<SideBar>();
-                    var courseManagment = sideBar.GoToCourseManagementPage();
-                    var coursesBlocks = courseManagment.GetBlocks();
-                    var courseItem = coursesBlocks.FirstOrDefault(x => x.GetCourseName().Equals("C# Essential", StringComparison.OrdinalIgnoreCase));
-                    courseItem.ClickCourseLink();
-
-                    var ListOfTasks = ConstructPage<TasksPage>();
-                    string TaskName = "Indexers";
-                    string nameOfExecutedTask = "";
-
-
-                    var blocks = ListOfTasks.GetBlocks();
-                    if (blocks != null)
-                    {
-                        var firstblock = blocks.FirstOrDefault(x => x.GetName().Equals(TaskName, StringComparison.OrdinalIgnoreCase));
-                        nameOfExecutedTask = firstblock.GetName();
-                        firstblock.ClickOnTasksButton();
-
-                        var TaskView = ConstructPage<TaskViewPage>();
-                        TaskView.ClickOnStartButton();
-
-                        var Code = ConstructPage<SolutionCodePage>();
-
-                        Code.ClickOnExecuteButton("Total");
-
-                    }
-                    var historyPage = sideBar.GoToCodeHistoryPage();
-                    var blocksHistory = historyPage.GetHistoryBlocks();
-
-                    bool blockOfExecutedCode = false;
-
-                    foreach (var block in blocksHistory)
-                    {
-                        if (block.GetTitle() == nameOfExecutedTask)
-                        {
-                            blockOfExecutedCode = true;
-                            break;
-                        }
-                    }
-                    Assert.True(blockOfExecutedCode);
-
+                    blockOfExecutedCode = true;
+                    break;
                 }
             }
-       
+
+            Assert.True(blockOfExecutedCode);
+        }
+    }
 }
