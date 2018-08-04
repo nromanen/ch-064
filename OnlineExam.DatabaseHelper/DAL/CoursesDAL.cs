93a4829d8023db5b1bff133c6b44fe35f80298be
m1shkka <mishamaxixi@gmail.com>
2018-08-05 01:20:15 +0300

    start creating api tests

---
+++ OnlineExam.DatabaseHelper/DAL/CoursesDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.DatabaseHelper.DAL
+{
+    public class CouresesDAL
+    {
+        private static string conection = "data source = DESKTOP-7VG2H7M\\SQLEXPRESS; initial catalog = OnlineExamDB; integrated security = True; MultipleActiveResultSets = True;";
+        //"Server=(localdb)\\mssqllocaldb;Database=Main;Trusted_Connection=True;MultipleActiveResultSets=true";
+        public Courses GetCommentById()
+        {
+            using (var ctx = new DataModel(conection))
+            {
+                var result = ctx.Courses.Find();
+                return result;
+            }
+        }
+
+        public Comments GetCommentByCommentText(string commentText)
+        {
+            using (var ctx = new DataModel(conection))
+            {
+                var result = ctx.Comments.First(c => c.CommentText == commentText);
+                // var result = ctx.Comments.Find(commentText);
+                return result;
+            }
+        }
+    }
+}
