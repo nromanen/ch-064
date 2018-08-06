79517a69f9f7e73f127174bb13828cbb760a0ce5
viktor <viktor140798@gmail.com>
2018-08-06 16:12:50 +0300

    add BaseAPITest

---
+++ OnlineExam.NUnitTests/APITests/BaseAPITest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OnlineExam.Framework;
+using OpenQA.Selenium;
+
+namespace OnlineExam.NUnitTests.APITests
+{
+    public abstract class BaseAPITest
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            ExtentTestManager.CreateParentTest(GetType().Name);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            ExtentManager.Instance.Flush();
+        }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            ExtentTestManager.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace)
+                ? ""
+                : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
+            var errorMessage = TestContext.CurrentContext.Result.Message;
+            Status logstatus;
+
+            switch (status)
+            {
+                case TestStatus.Failed:
+                    logstatus = Status.Fail;
+                    break;
+                case TestStatus.Inconclusive:
+                    logstatus = Status.Warning;
+                    break;
+                case TestStatus.Skipped:
+                    logstatus = Status.Skip;
+                    break;
+                default:
+                    logstatus = Status.Pass;
+                    break;
+            }
+
+            ExtentTestManager.GetTest().Log(logstatus,
+                "Test ended with " + logstatus + "\n<br>\n<br>  " + stacktrace + "\n<br>\n<br> " + errorMessage);
+        }
+    }
+}
