20eb0d807c848cc22af3b5f406cdd0c80508c3e8
Inna <inna.syrotiuk@gmail.com>
2018-08-06 20:45:56 +0300

    Add ignored file

---
+++ OnlineExam.NUnitTests/APIClients/Models/NewsAPIModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.NUnitTests.APIClients.Models
+{
+    public class NewsAPIModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string text { get; set; }
+        public int courseId { get; set; }
+    }
+}
