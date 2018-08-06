02f5b970de0cd20cc1ab800718429bddd5bb8d14
Surikenchik <popelyshkon25@gmail.com>
2018-08-06 19:57:36 +0300

    integreted security=true

--- OnlineExam.DatabaseHelper/Helper.cs
+++ OnlineExam.DatabaseHelper/Helper.cs
@@ -8,7 +8,7 @@ namespace OnlineExam.DatabaseHelper
     {
         // ༼ つ ಥ_ಥ ༽つ
         // Created by Roma Bahlai
-        private static string conection = $"Server = {BaseSettings.fields.ServerName}; Database = {BaseSettings.fields.DatabaseName};";
+        private static string conection = $"Server = {BaseSettings.fields.ServerName}; Database = {BaseSettings.fields.DatabaseName}; Integrated security = True;";
 
         public static void RollbackDatabase()
         {
