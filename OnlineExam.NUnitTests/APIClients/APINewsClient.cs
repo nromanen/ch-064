e0045a0557789fa3c8774e3f9c687caa5d906674
Inna <inna.syrotiuk@gmail.com>
2018-08-07 08:38:58 +0300

    Add missed files

---
+++ OnlineExam.NUnitTests/APIClients/APINewsClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OnlineExam.DatabaseHelper;
+using OnlineExam.NUnitTests.APIClients.Models;
+using RestSharp;
+
+namespace OnlineExam.NUnitTests.APIClients
+{
+    public class APINewsClient
+    {
+        RestClient client = new RestClient("http://localhost:55842");
+
+        public List<NewsAPIModel> Get()
+        {
+            var request = new RestRequest($"/api/news", Method.GET);
+            var response = client.Execute(request);
+            string jsonString = response.Content;
+            List<NewsAPIModel> result = JsonConvert.DeserializeObject<List<NewsAPIModel>>(jsonString);
+            return result;
+        }
+
+     
+
+        public HttpStatusCode Post(Object obj)
+        {
+            var request = new RestRequest($"/api/news", Method.POST);
+            request.AddHeader("content-type", "application/json");
+            request.AddJsonBody(obj);
+            var result = client.Execute(request);
+            return result.StatusCode;
+        }
+    }
+}
---
+++ OnlineExam.NUnitTests/APITests/APINewsTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OnlineExam.DatabaseHelper.DAL;
+using OnlineExam.NUnitTests.APIClients;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.NUnitTests.APITests
+{
+    [TestFixture]
+    public class APINewsTest
+    {
+        private APINewsClient client;
+
+        [Test]
+        public void GetNews()
+        {
+            
+            client = new APINewsClient();
+            var news = client.Get();
+            Assert.NotNull(news);
+           
+            var actual = new NewsDAL().GetNews();
+            
+            Assert.AreEqual(news.Count, actual.Count);
+            foreach (var x in actual)
+            {
+                var apiItem = news.FirstOrDefault(z => z.id == x.Id);
+                Assert.NotNull(apiItem);
+                StringAssert.AreEqualIgnoringCase(x.Title, apiItem.title);
+                StringAssert.AreEqualIgnoringCase(x.Text, apiItem.text);
+            }
+        }
+
+        [Test]
+        public void Post()
+        {
+            string title = Guid.NewGuid().ToString();
+
+            var obj = new
+            {
+                Text = "text",
+                Title = title,
+                Course = "C# Starter",
+            };
+            client = new APINewsClient();
+            var status = client.Post(obj);
+            Assert.AreEqual(HttpStatusCode.OK, status);
+
+            var actual = new NewsDAL().GetNewsByTitle(obj.Title.ToString());
+        
+            Assert.NotNull(actual);
+            StringAssert.AreEqualIgnoringCase(obj.Text, actual.Text);
+            StringAssert.AreEqualIgnoringCase(obj.Course, actual.Courses.Name);
+        }
+}
+}
