47900ce041cca88bcf683bd02424a75717518353
Surikenchik <popelyshkon25@gmail.com>
2018-08-09 15:24:52 +0300

    coments api fixed

--- OnlineExam.NUnitTests/APIClients/APICommentsClient.cs
+++ OnlineExam.NUnitTests/APIClients/APICommentsClient.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using OnlineExam.Framework;
+using OnlineExam.NUnitTests.APIClients.Models;
 using RestSharp;
 
 namespace OnlineExam.NUnitTests.APIClients
@@ -12,18 +15,27 @@ namespace OnlineExam.NUnitTests.APIClients
     {
         RestClient client = new RestClient(BaseSettings.Fields.Url);
 
-        public void Get(int commentId)
+
+        public RestResultTyped<List<CommentsAPIModel>> Get(int commentId)
         {
             var request = new RestRequest($"/api/CommentApi/{commentId}", Method.GET);
             var response = client.Execute(request);
+            string jsonString = response.Content;
+            List<CommentsAPIModel> result = JsonConvert.DeserializeObject<List<CommentsAPIModel>>(jsonString);
+            return new RestResultTyped<List<CommentsAPIModel>>()
+            {
+                Code = response.StatusCode,
+                Data = result
+            };
         }
 
-        public void Post(Object obj)
+        public HttpStatusCode Post(Object obj)
         {
             var request = new RestRequest($"/api/CommentApi", Method.POST);
             request.AddHeader("content-type", "application/json");
             request.AddJsonBody(obj);
-            client.Execute(request);
+            var result = client.Execute(request);
+            return result.StatusCode;
         }
     }
 }
--- OnlineExam.NUnitTests/APIClients/Models/CommentsAPIModel.cs
+++ OnlineExam.NUnitTests/APIClients/Models/CommentsAPIModel.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace OnlineExam.NUnitTests.APIClients.Models
 {
-    class CommentsAPIModel
+    public class CommentsAPIModel
     {
+        public int Id { get; set; }
+
+        public string CommentText { get; set; }
+
+        public DateTime CreationDateTime { get; set; }
+
+        public int ExerciseId { get; set; }
+
+        public int Rating { get; set; }
+
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
     }
 }
