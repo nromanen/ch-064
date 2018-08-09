de99a3a645ca1e4d1d77a8ece5361e64fb0ef6b3
viktor <viktor140798@gmail.com>
2018-08-09 16:19:02 +0300

    test progress

--- OnlineExam.NUnitTests/BaseNTest.cs
+++ OnlineExam.NUnitTests/BaseNTest.cs
@@ -125,7 +125,7 @@ namespace OnlineExam.NUnitTests
 
             ExtentTestManager.GetTest().Log(logstatus,
                 "Test ended with " + logstatus + "\n<br>\n<br>  " + stacktrace + "\n<br>\n<br> " + errorMessage +
-                "\n<br>\n<br> " + TestContext.Progress.Encoding);
+                "\n<br>\n<br> " + TestContext.Progress);
 
             driver?.Dispose();
         }
