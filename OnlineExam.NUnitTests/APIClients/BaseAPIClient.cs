29843974aae0d630f653005e49fc474d5eefdeb0
viktor <viktor140798@gmail.com>
2018-08-15 15:59:58 +0300

    added token

--- OnlineExam.NUnitTests/APIClients/BaseAPIClient.cs
+++ OnlineExam.NUnitTests/APIClients/BaseAPIClient.cs
@@ -11,5 +11,20 @@ namespace OnlineExam.NUnitTests.APIClients
     public class BaseAPIClient
     {
         public RestClient client = new RestClient(BaseSettings.Fields.Url);
+
+        public string GetToken(string email,string password)
+        {
+            var loginRequest = new RestRequest("/token", Method.POST);
+            loginRequest.AddParameter("grant_type", "password");
+            loginRequest.AddParameter("username", email);
+            loginRequest.AddParameter("password", password);
+            var res = client.Execute<AuthResult>(loginRequest);
+            return res.Data.access_token;
+        }
+
+        public class AuthResult
+        {
+            public string access_token { get; set; }
+        }
     }
 }
