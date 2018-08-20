972877e812964c98c547a12f495f594e5a090725
m1shkka <mishamaxixi@gmail.com>
2018-08-20 17:47:28 +0300

    add json files for resolving add "ing" for LogProgress

---
+++ OnlineExam.Framework/Params/TaskViewPageParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Framework.Params
+{
+    public class TaskViewPageParams
+    {
+        public string TaskName {get;set;}
+        public string CourseName {get;set;}
+        public string Email {get;set;}
+        public string Comment {get;set;}
+        public string Date {get;set;}
+        public string Stars {get;set;}
+        public string Url {get;set;}
+        public string BasseTitle{get;set;}
+    }
+}
